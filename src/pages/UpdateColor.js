02498bb0ef004842e9a6e6b380d88f0a7b1da701
Robert Hammarstrøm <robert.hammarstroem@sparebank1.no>
2023-02-15 23:15:49 +0100

    Update color

--- src/pages/UpdateColor.js
+++ src/pages/UpdateColor.js
@@ -1,12 +1,11 @@
 
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import DarkButton from "../interact/button/button";
 import {Form} from "react-bootstrap";
 import "./UpdateColor.css";
-import {ownerOf, updateColor} from "../interact/wallet/wallet";
+import {ownerOf, updateColor, getMetadata} from "../interact/wallet/wallet";
 import {Alert} from "react-bootstrap";
 import MoreInfoColorChange from "./MoreInfoColorChange";
-import { ChromePicker } from 'react-color';
 
 function UpdateColor({connectedAdress}) {
 
@@ -22,12 +21,11 @@ function UpdateColor({connectedAdress}) {
   const [isLoading, setIsLoading] = useState(false);
 
 
-  const [sketchPickerColor, setSketchPickerColor] = useState({
-    r: "241",
-    g: "112",
-    b: "19",
-    a: "1",
-  });
+    useEffect(() => {
+        if(connectedAdress !== ""){
+            setInputErrorMessage("")
+        }
+    },[connectedAdress])
 
   function isValidColor(str) {
     return /^[0-9A-F]{6}$/i.test(str);
@@ -37,39 +35,57 @@ function UpdateColor({connectedAdress}) {
     let text = e.target.value
 
     if(e.target.name === 'TokenId'){
-
-      setTokenId(text);
+      setTokenId(text)
+      setInputErrorMessage("")
       return;
     }
 
-    if(text.length === 0){
-      setValidated(true);
-      setIsValidHexColor(true);
-    }
-    else if(!isValidColor(text)){
-        setValidated(true);
-        setIsValidHexColor(false);
-        setInputErrorMessage(`${e.target.name} input is not valid Hex color. Please provide a valid Hex color without the #`);
-    }
-    else{
-      setIsValidHexColor(true);
-      setValidated(true);
-    }
-
     if(e.target.name === 'Head'){
-      setHeadColor(text);
+      setHeadColor(text.replace('#', ''));
+
     }
     else if(e.target.name === 'Eye'){
-      setEyeColor(text);
+      setEyeColor(text.replace('#', ''));
     }
     else if(e.target.name === 'Mouth'){
-      setMouthColor(text);
+      setMouthColor(text.replace('#', ''));
     }
     else if(e.target.name === 'Text'){
-      setTextColor(text);
+      setTextColor(text.replace('#', ''));
     }
+
 }
 
+const handleGetMetaData = () => {
+        if(tokenId === ""){
+            setInputErrorMessage(`Please provide an token id`);
+            setValidated(true);
+            setIsValidHexColor(false);
+        }
+        try {
+            getMetadata(tokenId).then((m) => {
+                if (m.includes("URI query for nonexistent")) {
+                    setInputErrorMessage(`No Pepe with token id ${tokenId}`);
+                    setValidated(true);
+                    setIsValidHexColor(false);
+                } else {
+                    let base64Metadata = m.replace("data:application/json;base64,", "");
+                    let decodedData = JSON.parse(Buffer.from(base64Metadata, 'base64').toString('utf8'))
+                    setHeadColor(decodedData.attributes.filter(item => item.trait_type === 'Head color')[0].value.replace('#', ''))
+                    setTextColor(decodedData.attributes.filter(item => item.trait_type === 'Text color')[0].value.replace('#', ''))
+                    setMouthColor(decodedData.attributes.filter(item => item.trait_type === 'Mouth color')[0].value.replace('#', ''))
+                    setEyeColor(decodedData.attributes.filter(item => item.trait_type === 'Eye color')[0].value.replace('#', ''))
+                }
+            })
+        }
+        catch (e){
+            if(e.message.includes("URI query for nonexistent")){
+                setInputErrorMessage(`No Pepe with token id ${tokenId}`);
+                setValidated(true);
+                setIsValidHexColor(false);
+            }
+        }
+}
 
 const handleUpdateColor = () => {
 
@@ -101,7 +117,6 @@ const handleUpdateColor = () => {
                   setInputErrorMessage(`Please connect wallet`);
               }
                   else if(owner.toLowerCase() === connectedAdress.toLowerCase()){
-
                       updateColor(tokenId, headColor, eyeColor, textColor, mouthColor, setTxHash, setIsLoading)
                           .then()
                   }
@@ -126,84 +141,89 @@ const handleUpdateColor = () => {
       <div className="colorGrid">                        
         <Form variant="dark">
               <Form.Group className="mb-3" controlId="exampleForm.ControlInput1"  variant="dark">
-                  <Form.Label></Form.Label>
-                  <Form.Control
-                      name="TokenId"
-                      size="sm"    
-                      variant="dark"                            
-                      placeholder="Token Id"
-                      onChange={handleInputChange}
-                      maxLength={3}  
-                      type="text"
-                      onKeyPress={(event) => {
-                        if (!/[0-9]/.test(event.key)) {
-                          event.preventDefault();
-                        }
-                      }}
-                
-                  />
-                  <Form.Label></Form.Label>
-                  <Form.Control         
-                      name="Head"       
-                      size="sm"                                
-                      placeholder="Head color"    
-                      variant="dark"
-                      onChange={handleInputChange}                                        
-                      required     
-                      type="text"
-                      maxLength={6}                                        
-                      {...(isValidHexColor && validated ? {className:"is-valid"} : {})}
-                      {...(!isValidHexColor && validated ? {className:"is-invalid"} : {})}
-                  />
-                  <Form.Label></Form.Label>
-                  <Form.Control
-                      name="Eye"
-                      size="sm"    
-                      variant="dark"                            
-                      placeholder="Eye color"                                        
-                      type="text"
-                      maxLength={6}
-                      onChange={handleInputChange}
-                      {...(isValidHexColor && validated ? {className:"is-valid"} : {})}
-                      {...(!isValidHexColor && validated ? {className:"is-invalid"} : {})}                                   
-                  />
-                  <Form.Label></Form.Label>
-                  <Form.Control
-                      name="Text"
-                      size="sm"    
-                      variant="dark"                            
-                      placeholder="Text color"                                        
-                      type="text"
-                      maxLength={6}
-                      onChange={handleInputChange}
-                      {...(isValidHexColor && validated ? {className:"is-valid"} : {})}
-                      {...(!isValidHexColor && validated ? {className:"is-invalid"} : {})}                                   
-                  />
-                  <Form.Label></Form.Label>
-                  <Form.Control
-                      name="Mouth"
-                      size="sm"    
-                      variant="dark"                            
-                      placeholder="Mouth color"                                        
-                      type="text"
-                      maxLength={6}
-                      onChange={handleInputChange}
-                      {...(isValidHexColor && validated ? {className:"is-valid"} : {})}
-                      {...(!isValidHexColor && validated ? {className:"is-invalid"} : {})}                                   
-                  />
+                  <div className="color-buttons">
+                      <Form.Label>Token Id</Form.Label>
+                      <Form.Control
+                          name="TokenId"
+                          size="sm"
+                          className="colorbutton"
+                          variant="dark"
+                          placeholder="Token Id"
+                          onChange={handleInputChange}
+                          maxLength={3}
+                          type="text"
+                          onKeyPress={(event) => {
+                            if (!/[0-9]/.test(event.key)) {
+                              event.preventDefault();
+                            }
+                          }}
+
+                      />
+                  </div>
+                  <div className="color-buttons">
+                      <Form.Label color="white">Head</Form.Label>
+                      <Form.Control
+                          name="Head"
+                          size="sm"
+                          value={'#' + headColor}
+                          className="colorbutton"
+                          placeholder="Head color"
+                          variant="dark"
+                          onChange={handleInputChange}
+                          required
+                          type="color"
+                          maxLength={6}
+                      />
+                  </div>
+                  <div className="color-buttons">
+                      <Form.Label>Eyes</Form.Label>
+                      <Form.Control
+                          name="Eye"
+                          size="sm"
+                          variant="dark"
+                          className="colorbutton"
+                          value={'#' + eyeColor}
+                          placeholder="Eye color"
+                          type="color"
+                          maxLength={6}
+                          onChange={handleInputChange}
+                      />
+                  </div>
+                  <div className="color-buttons">
+                      <Form.Label>Text</Form.Label>
+                      <Form.Control
+                          name="Text"
+                          size="sm"
+                          variant="dark"
+                          className="colorbutton"
+                          placeholder="Text color"
+                          value={'#' + textColor}
+                          type="color"
+                          maxLength={6}
+                          onChange={handleInputChange}
+                      />
+                  </div>
+                  <div className="color-buttons">
+                      <Form.Label>Mouth</Form.Label>
+                      <Form.Control
+                          name="Mouth"
+                          size="sm"
+                          variant="dark"
+                          className="colorbutton"
+                          placeholder="Mouth color"
+                          value={'#' + mouthColor}
+                          type="color"
+                          maxLength={6}
+                          onChange={handleInputChange}                      />
+                  </div>
                   <Form.Control.Feedback type="invalid">{inputErrorMessage}</Form.Control.Feedback>
               </Form.Group>
-            <div className='color-buttons'>
-                <DarkButton size={'lm'} onClickFunction={handleUpdateColor} disableIf={isLoading} text={isLoading ? 'Updating…' : `Update Color`}></DarkButton>
-                  <MoreInfoColorChange></MoreInfoColorChange>
+            <div className='update-buttons'>
+                <DarkButton size={'lm'} onClickFunction={handleUpdateColor} disableIf={isLoading} text={isLoading ? 'Updating… ' : `Update Color`}></DarkButton>
+                <DarkButton size={'lm'} onClickFunction={handleGetMetaData} disableIf={isLoading} text={isLoading ? 'Updating… ' : `Fetch Metadata`}></DarkButton>
+                <MoreInfoColorChange></MoreInfoColorChange>
             </div>
           </Form>
-          <ChromePicker
-          onChange={(color) => {
-            setSketchPickerColor(color.rgb);
-          }}
-          color={sketchPickerColor}
-        />
 
         </div>
             {txHash !== "" &&  <Alert variant='dark' onClose={() => setTxHash("")} dismissible>

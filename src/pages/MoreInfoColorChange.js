fc268c8920b68be537ea4d2a6f70a2ef9295cda8
Robert Hammarstrøm <robert.hammarstroem@sparebank1.no>
2023-02-17 15:04:13 +0100

    info revert

--- src/pages/MoreInfoColorChange.js
+++ src/pages/MoreInfoColorChange.js
@@ -9,17 +9,16 @@ function MoreInfoColorChange() {
         <Popover id="popover-basic">
         <Popover.Header as="h3">Update Color</Popover.Header>
         <Popover.Body>
-        A Pepe can be customized by changing its color.
-        You can load existing colors for a Pepe by fetching its metadata.
-        Changing color is free, but since metadata is updated On Chain you have to pay gas.
-        If changing color directly from contract, it must be provided by its 6 character hexadecimal representation without the #.
+        You can customize your Pepe by changing its color. The color must be provided by its 6 character hexadecimal representation without the #.
+            If you want to keep a color, you can just leave that field blank.
+            Changing color is free, but since metadata is updated On Chain you have to pay gas.
         </Popover.Body>
       </Popover>
       );
 
     return (
         <OverlayTrigger trigger="click" rootClose placement="right" overlay={popover}>
-        <Button size={'lg'} variant="dark"><img src={info} alt="my"/></Button>
+        <Button variant="dark"><img src={info} alt="my"/></Button>        
         </OverlayTrigger>
     );
 }
